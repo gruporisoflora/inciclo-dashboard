114bc8e48760a15b3e9f50312bb5395009e0017f
OtacilioN <contato@otaciliomaia.com>
2019-05-09 12:24:06 -0300

    Add build version

---
+++ build/precache-manifest.d67a73135c74357ce5b6147342ee9ba9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dcc32936a6d3982e6ae742b32ef25da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94f521786a1eae435c42",
+    "url": "/static/css/main.0ac34472.chunk.css"
+  },
+  {
+    "revision": "0b5439a346ad198a1276",
+    "url": "/static/js/2.024f283b.chunk.js"
+  },
+  {
+    "revision": "94f521786a1eae435c42",
+    "url": "/static/js/main.97765178.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4db2b97962bcf7dee1efb5d76de7c62c",
+    "url": "/static/media/background.4db2b979.png"
+  }
+]);
