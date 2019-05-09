f259efb382678f90be0ec8e5bcbd155189b723be
OtacilioN <contato@otaciliomaia.com>
2019-05-09 12:10:51 -0300

    Updates

---
+++ precache-manifest.c499838ecaabf9178b482c59c3333db3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca47318cc8d727fd91178b0e95639be5",
+    "url": "/inciclo-dashboard/index.html"
+  },
+  {
+    "revision": "d8168b49972182fc29ba",
+    "url": "/inciclo-dashboard/static/css/main.0ac34472.chunk.css"
+  },
+  {
+    "revision": "0b5439a346ad198a1276",
+    "url": "/inciclo-dashboard/static/js/2.024f283b.chunk.js"
+  },
+  {
+    "revision": "d8168b49972182fc29ba",
+    "url": "/inciclo-dashboard/static/js/main.a56efbab.chunk.js"
+  },
+  {
+    "revision": "056607ff4413479a90dd",
+    "url": "/inciclo-dashboard/static/js/runtime~main.12449edc.js"
+  },
+  {
+    "revision": "4db2b97962bcf7dee1efb5d76de7c62c",
+    "url": "/inciclo-dashboard/static/media/background.4db2b979.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/myapp/precache-manifest.91dc763c5d56d4033faf0a452f6618ee.js"
+  "/inciclo-dashboard/precache-manifest.c499838ecaabf9178b482c59c3333db3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/myapp/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/inciclo-dashboard/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
