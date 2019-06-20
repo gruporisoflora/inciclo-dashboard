e49218be4bf285044e140ed985d0c4b1f965ad08
OtacilioN <contato@otaciliomaia.com>
2019-06-20 10:53:35 -0300

    Updates

---
+++ precache-manifest.ef52df29ee6c58157834bbb9f08a911e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6347392b79a860455b3d41693069c886",
+    "url": "/inciclo-dashboard/index.html"
+  },
+  {
+    "revision": "ba2ab835aa5c3074d8ca",
+    "url": "/inciclo-dashboard/static/css/main.b9f73580.chunk.css"
+  },
+  {
+    "revision": "722f772446fbd2dee88f",
+    "url": "/inciclo-dashboard/static/js/2.d417d8f1.chunk.js"
+  },
+  {
+    "revision": "ba2ab835aa5c3074d8ca",
+    "url": "/inciclo-dashboard/static/js/main.184c98c7.chunk.js"
+  },
+  {
+    "revision": "056607ff4413479a90dd",
+    "url": "/inciclo-dashboard/static/js/runtime~main.12449edc.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/inciclo-dashboard/precache-manifest.37f132b0b63a3bb709efe92315514559.js"
+  "/inciclo-dashboard/precache-manifest.ef52df29ee6c58157834bbb9f08a911e.js"
 );
 
 self.addEventListener('message', (event) => {
